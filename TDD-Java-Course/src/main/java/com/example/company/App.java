b6cd5b23b82e8af47503ea6bd5097a6a7377f103
LeonardoGalicia <76215859+LeonardoGalicia@users.noreply.github.com>
2021-01-18 13:12:48 -0600

    pruebas unitaria ejercicio fizzBuzz

--- TDD-Java-Course/src/main/java/com/example/company/App.java
+++ TDD-Java-Course/src/main/java/com/example/company/App.java
@@ -1,5 +1,10 @@
 package com.example.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+
+    public static List<String> fizzBuzz(int inputNumber) {
+        if(inputNumber<=0 || inputNumber >100){
+            throw new IllegalArgumentException("no puede ser menor a cero ni mayor a 100");
+        }
+        return IntStream.rangeClosed(1, inputNumber)
+                .mapToObj(i -> i % 5 == 0 ? (i % 3 == 0 ? "FizzBuzz" : "Buzz") : (i % 3 == 0 ? "Fizz" : i+""))
+                .collect(Collectors.toList());
     }
 }
--- TDD-Java-Course/src/test/java/com/example/company/AppTest.java
+++ TDD-Java-Course/src/test/java/com/example/company/AppTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class AppTest 
 {
     @Test
@@ -18,4 +20,54 @@ public class AppTest
 
         Assert.assertTrue(true);
     }
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowAnExceptionWhenGetANegativeNumber(){
+        int inputNumber =-1;
+        App.fizzBuzz(inputNumber);
+    }
+
+    @Test
+    public void shouldGetAFizzBuzzWhenNumber15() {
+
+        int inputNumber = 20;
+        List<String> result = App.fizzBuzz(inputNumber);
+        Assert.assertEquals("FizzBuzz",result.get(14));
+    }
+
+    @Test
+    public void shouldGet4WhenNumberPosition3() {
+
+        int inputNumber = 20;
+        List<String> result = App.fizzBuzz(inputNumber);
+        Assert.assertEquals("4",result.get(3));
+    }
+
+    @Test
+    public void shouldGetBuzzWhenNumberPosition4() {
+
+        int inputNumber = 20;
+        List<String> result = App.fizzBuzz(inputNumber);
+        Assert.assertEquals("Buzz",result.get(4));
+    }
+
+    @Test
+    public void shouldGetFizzWhenNumberPosition2() {
+
+        int inputNumber = 20;
+        List<String> result = App.fizzBuzz(inputNumber);
+        Assert.assertEquals("Fizz",result.get(2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldLimit() {
+        int inputNumber = 101;
+        App.fizzBuzz(inputNumber);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void should() {
+        int inputNumber = 0;
+        App.fizzBuzz(inputNumber);
+    }
+
+
 }
